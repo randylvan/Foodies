db73d013f99fe74f8424e124bb0fcf9ac0652490
rick baker <baker.rickw@gmail.com>
2017-03-16 17:29:54 -0600

    added action code

--- client/actions/categories.js
+++ client/actions/categories.js
@@ -1,6 +1,17 @@
 import { setFlash } from './flash';
 
 // get all categories
+export const getCategories = () => {
+  return (dispatch) => {
+    $.ajax({
+      url: '/api/categories',
+      type: 'GET'
+    }).done( categories => {
+      dispatch({ type: 'CATEGORIES', categories })
+    });
+  }
+}
+
 // get enabled categories
 // enable category
 // disable category
