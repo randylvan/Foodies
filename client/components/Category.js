75d7c6d982ae3ecc26b6ca4758f2c0dd13cd41e5
rick baker <baker.rickw@gmail.com>
2017-03-20 17:17:15 -0600

    changes

--- client/components/Category.js
+++ client/components/Category.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { getCategories }  from '../actions/categories'
 
 class Category extends React.Component {
     constructor(props) {
