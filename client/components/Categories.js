cb9fd290a95bc865fd7ed275469efc0394d24b44
rick baker <baker.rickw@gmail.com>
2017-03-23 19:56:58 -0600

    more clean up

--- client/components/Categories.js
+++ client/components/Categories.js
@@ -10,33 +10,8 @@ class Categories extends React.Component {
         this.props.dispatch(getCategories()); //Call  ./action/categories.js
     }
 
-    // toggleEnable = () => {
-    //     console.log("toggle this");
-    // }
-
-    // updateCategory = (category) => {
-    //     // console.log("update category here")
-    //     // ** May use this for updating enable state to database **
-
-    //     // let { _id, name } = category;
-    //     // $.ajax({
-    //     // url: `/category/${_id}`,
-    //     // type: 'PUT',
-    //     // data: { enabled }
-    //     // }).done( category => {
-    //     // let categories = this.state.categories.map( c => {
-    //     //     if (c._id === _id)
-    //     //     return category
-    //     //     return c
-    //     // });
-
-    //     // this.setState({ categories });
-    //     // });
-    // }
-
     render () {
         let categoryList = this.props.categories.map( category => {
-        // console.log(category._id)
             return (
                 <div key={category._id} className="col s12 m4 l4">
                     <Category
