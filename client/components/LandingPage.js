761715f5e113af73ea76346e10a72d39be492739
rick baker <baker.rickw@gmail.com>
2017-04-03 20:01:58 -0600

    add image

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -9,14 +9,12 @@ class LandingPage extends React.Component {
     <div>
         
         <div className="site-wrapper">
-        <div className="img"></div>
+        {/*<div className="img"></div>*/}
       <div className="site-wrapper-inner">
 
         <div className="cover-container">
 
           <div className="inner cover center container">
-            <h1 className="cover-heading center white-text">Foodie<span className="red-text">Tinder</span></h1>
-            <p className="lead center white-text">The only application you'll need to find your lucky food <em className="green-text"><b>TODAY</b></em></p>
             <p className="lead">
               <Link to={'/signUp'} className="btn btn-lg btn-default red">Sign up</Link>
             </p>
