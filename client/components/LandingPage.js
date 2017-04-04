120f076a6861669b9aaa0be6e6fa4aa3e0bc4de0
Randy Van <randyvanprojets@gmail.com>
2017-04-03 20:02:54 -0600

    resolve loading page

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -7,36 +7,22 @@ class LandingPage extends React.Component {
     render() {    
         return (
     <div>
-        
-        <div className="site-wrapper">
-      {/*<div className="img"></div>*/}
-      <div className="site-wrapper-inner">
+      <div class="section no-pad-bot" id="index-banner">
+          <div class="container">
 
-        <div className="cover-container">
-
-          <div className="inner cover center container">
-            <h1 className="cover-heading center white-text">Foodie<span className="red-text">Tinder</span></h1>
-            <p className="lead center white-text">The only application you'll need to find your lucky food <em className="green-text"><b>TODAY</b></em></p>
-            <p className="lead">
-              <Link to={'/signUp'} className="btn btn-lg btn-default red">Sign up</Link>
-            </p>
-          </div>
-
-          <div className="mastfoot center">
-            <div className="inner">
-              <p className="white-text">&copy; Foodie<span className="red-text">Tinder</span></p>
-            </div>
-          </div>
-
+         <h1 class="header center orange-text">Starter Template</h1>
+        <div class="row center">
+        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+      </div>
+        <div class="row center">
+          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
         </div>
 
       </div>
-
+      </div>
     </div>
-
-    </div>
-)
-}
+    )
+  }
 };
 
 export default LandingPage;
